d164234860de1031c5f30d36f5c4a17b387d8a34
신희준 <shinhj88@gmail.com>
2020-01-21 01:01:02 +0900

    boj7469 완료

--- boj7469/solve.go
+++ boj7469/solve.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 type MergeSort struct{
@@ -23,23 +21,31 @@ func NewMergetSort(data *[]int)*MergeSort{
 
 }
 func (mdata *MergeSort)merge(leftnode,rightnode,node int){
-	i ,j:=0,0
+	i ,j,k:=0,0,0
+	lsize := len(mdata.Tree[leftnode])
+	rsize := len(mdata.Tree[rightnode])
+	mdata.Tree[node] = make([]int,lsize+rsize)
+
 	for; i < len(mdata.Tree[leftnode]) && j < len(mdata.Tree[rightnode]);{
 		if mdata.Tree[leftnode][i] < mdata.Tree[rightnode][j]{
-			mdata.Tree[node] = append(mdata.Tree[node],mdata.Tree[leftnode][i])
+			mdata.Tree[node][k]=mdata.Tree[leftnode][i]
 			i++
+			k++
 
 		}else{
-			mdata.Tree[node] = append(mdata.Tree[node],mdata.Tree[rightnode][j])
+			mdata.Tree[node][k] = mdata.Tree[rightnode][j]
 			j++
+			k++
 
 		}
 	}
 	for ; i < len(mdata.Tree[leftnode]); i++{
-		mdata.Tree[node] = append(mdata.Tree[node],mdata.Tree[leftnode][i])
+		mdata.Tree[node][k] = mdata.Tree[leftnode][i]
+		k++
 	}
 	for ; j< len(mdata.Tree[rightnode]); j++{
-		mdata.Tree[node] = append(mdata.Tree[node],mdata.Tree[rightnode][j])
+		mdata.Tree[node][k] =mdata.Tree[rightnode][j]
+		k++
 	}
 }
 func (mdata *MergeSort)init(data *[]int,start,end ,node int){
@@ -85,7 +91,6 @@ func upper_boud(arr *[]int,end,value int)int{
 	for; end - start > 0;{
 		mid = int((start+end)/2)
 
-
 		if((*arr)[mid] <= value){
 			start = mid + 1
 		}else{
@@ -94,82 +99,48 @@ func upper_boud(arr *[]int,end,value int)int{
 	}
 	return end +1
 }
-
-func main() {
-
-	reader := bufio.NewScanner(os.Stdin)
-	wrieter := bufio.NewWriter(os.Stdout)
-	var N,Q int;
-
-	step := 0
-	var t *MergeSort
-	var l,r,v int
-	i :=0
-	run := 0
-	finish := false
-	var data []int
-	for ;finish != true; {
-		reader.Scan()
-		line := reader.Text()
-		s := strings.Split(line, " ")
-
-		for j := 0; j < len(s); j++ {
-			if step == 0 {
-				if i == 0 {
-					N, _ = strconv.Atoi(s[j])
-				}
-				if i == 1 {
-					Q, _ = strconv.Atoi(s[j])
-
-				}
-				i++
-				if i == 2 {
-					step++
-					i = 0
-
-				}
-			} else if step == 1 {
-				if i== 0{
-					data = make([]int,N)
-				}
-				data[i], _ = strconv.Atoi(s[j])
-				i++
-				if i == N {
-					i = 0
-					t = NewMergetSort(&data)
-					step++
-
-				}
-			} else if step == 2 {
-				if i == 0 {
-					l, _ = strconv.Atoi(s[j])
-				}
-				if i == 1 {
-					r, _ = strconv.Atoi(s[j])
-				}
-				if i == 2 {
-					v, _ = strconv.Atoi(s[j])
-				}
-				i++
-				if i == 3 {
-
-					i = 0
-					l--
-					r--
-
-					fmt.Fprintf(wrieter, "%d\n", t.qeuryanw(l, r, v))
-					run++
-					if run == Q {
-						finish = true
-					}
-
-				}
-			}
-		}
+func toInt(buf []byte) (n int) {
+	for _, v := range buf {
+		n = n*10 + int(v-'0')
 	}
+	return
+}
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
+func scanf(f string, a ...interface{}) { fmt.Fscanf(reader, f, a...) }
+func main() {
+	defer writer.Flush()
 
 
 
-	wrieter.Flush()
-	
+	var N,Q int
+	scanf("%d%d\n",&N,&Q)
+	data := make([]int,N)
+
+	for i := 0; i < N; i++{
+		//fmt.Sscanf(reader.Text(),"%d",&data[i])
+		scanf("%d",&data[i])
+	}
+	scanf("\n")
+	t := NewMergetSort(&data)
+
+
+	for;Q>0;Q--{
+
+		//s = strings.Split(line," ")
+		//l,_ := strconv.Atoi(s[0])
+		//r,_ := strconv.Atoi(s[1])
+		//v,_ := strconv.Atoi(s[2])
+		var l,r,v int
+		scanf("%d%d%d\n",&l,&r,&v)
+		l--
+		r--
+
+		printf("%d\n", t.qeuryanw(l,r,v))
+
+
+	}
+	//wrieter.Flush()
+
 }
